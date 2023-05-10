7383b33cd450e9e2daf7a02e16cdbb620bcf0af5
anyaturing <>
2023-05-10 15:54:56 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# GPS Pet Tracker
+
+A tracker to track my cute German Shepherd.
+Hardware required:
+- SIRF III Satellite chip with 1200 BAUD serial output
+- Arduino board
+- Coil Antenna
+- 800 mAH battery
+
+
---
+++ tracker.py
@@ -0,0 +1,80 @@
+import MySQLdb
+import sys
+import os
+import serial
+from datetime import datetime
+
+try:
+	# Remove the hard coded creds and add a function to capture credentials
+	con = MySQLdb.connect('antur90.us-east-1.rds.amazonaws.com', 'aturing', 'Q91h2^oapsAN', 'tracker')
+	cur = con.cursor()
+except MySQLdb.Error:
+	print ("Error: ", MySQLdb.Error)
+	sys.exit(1)
+
+try:
+	print ("Trying... serial port...")
+	tracker = serial.Serial('COM3', 1200, timeout = 2) 
+except: 
+	print ("Failed to connect")
+	sys.exit(1)
+
+line = ""
+lat = ""
+lng = ""
+age = 0
+timelast = datetime.now()
+
+print ("Listening:")	
+while True:
+	try:
+		for b in tracker.read():
+			timelast = datetime.now()
+			if (b == ':'):
+				if len(line)>0: ## clear old saved data for new data
+					print("Fragmented Data: " + line)
+					pieces = line.split(",")
+					if len(pieces)==2:
+						lat = pieces[0]
+						lng = pieces[1]
+						pieces = lng.split("#")
+						if len(pieces)==2:
+							lng = pieces[0]
+							age = pieces[1]
+				
+					cur.execute("INSERT INTO evarate (raw,lat,lng,age) VALUES (%s,%s,%s,%s)", (line,lat,lng,age))
+					con.commit() #commit the insert
+					
+					line = ""
+					lat = ""
+					lng = ""
+					age = 0
+			else:
+				line+=b
+
+		tdelta = datetime.now() - timelast
+		tdelta = tdelta.total_seconds()
+		if ((tdelta>3) & (len(line)>0)):
+			timelast = datetime.now()
+			print("")
+			print("Recieved Data: " + line)
+			pieces = line.split(",")
+			if len(pieces)==2:
+				lat = pieces[0]
+				lng = pieces[1]
+				pieces = lng.split("#")
+				if len(pieces)==2:
+					lng = pieces[0]
+					age = pieces[1]
+		
+			cur.execute("INSERT INTO evarate (raw,lat,lng,age) VALUES (%s,%s,%s,%s)", (line,lat,lng,age))
+			con.commit() #commit the insert
+			
+			line = ""
+			lat = ""
+			lng = ""
+			age = 0
+		else:
+			print ("."),
+	except:
+		print ("Failed")
